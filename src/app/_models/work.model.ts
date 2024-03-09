0a776a1502007f42b7e939a54e1d302f5439c00a
NicoloCasagrande <nicolo.casagrande7@gmail.com>
2024-03-09 19:20:26 +0100

    fix tools per lista works

--- src/app/_models/work.model.ts
+++ src/app/_models/work.model.ts
@@ -3,10 +3,12 @@ export class Work {
     description: string;
     link: string;
     github_link: string;
-    constructor(name: string, description:string, link:string, github_link:string){
+    tools:string[]
+    constructor(name: string, description:string, link:string, github_link:string, tools:string[]){
         this.name = name;
         this.description = description;
         this.link = link;
         this.github_link = github_link;
+        this.tools = tools;
     }
 }
--- src/app/works/works.component.ts
+++ src/app/works/works.component.ts
@@ -15,11 +15,10 @@ export class WorksComponent implements OnInit{
   works: Work[] = [];
 
   ngOnInit(){
-    this.works.push(new Work('Enterprise Chat Reproduction', 'Creation of an Enterprise chat for employee only', 'https://nicolocasagrande.github.io/chat-reactjs/', 'https://github.com/NicoloCasagrande/chat-reactjs'));
-    this.works.push(new Work('Generic Dashboard', 'Creation of a generic dashboard template.', 'https://darling-buttercream-2e256f.netlify.app/', 'https://github.com/NicoloCasagrande/html-css-bootstrap-dashboard'));
-    this.works.push(new Work('Zoom Reproduction', 'Reproduction of a Zoom videocall.', 'https://silver-paletas-9caf29.netlify.app/', 'https://github.com/NicoloCasagrande/html-css-zoom'));
-    this.works.push(new Work('Whatsapp Reproduction', 'Reproduction of Whatsapp using an API that response with Chuck Norris phrases.', 'https://melodious-marshmallow-b9191a.netlify.app/', 'https://github.com/NicoloCasagrande/vue-boolzapp'));
-    this.works.push(new Work('Dropbox Home Page Reproduction', 'Reproduction of the Old Dropbox Website Home Page.', 'https://marvelous-genie-d025cb.netlify.app/', 'https://github.com/NicoloCasagrande/htmlcss-dropbox'));
+    this.works.push(new Work('Enterprise Chat Reproduction', 'Creation of an Enterprise chat for employee only', 'https://nicolocasagrande.github.io/chat-reactjs/', 'https://github.com/NicoloCasagrande/chat-reactjs', ['REACT', 'NODEJS']));
+    this.works.push(new Work('Generic Dashboard', 'Creation of a generic dashboard template.', 'https://darling-buttercream-2e256f.netlify.app/', 'https://github.com/NicoloCasagrande/html-css-bootstrap-dashboard', ['HTML', 'CSS']));
+    this.works.push(new Work('Whatsapp Reproduction', 'Reproduction of Whatsapp using an API that response with Chuck Norris phrases.', 'https://melodious-marshmallow-b9191a.netlify.app/', 'https://github.com/NicoloCasagrande/vue-boolzapp', ['VUE', 'VITE']));
+    this.works.push(new Work('Dropbox Home Page Reproduction', 'Reproduction of the Old Dropbox Website Home Page.', 'https://marvelous-genie-d025cb.netlify.app/', 'https://github.com/NicoloCasagrande/htmlcss-dropbox', ['HTML', 'CSS']));
     
   }
 
