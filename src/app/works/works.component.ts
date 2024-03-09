bcd2e95d75d26783ad46fccaad3e50dc22fb6e26
NicoloCasagrande <nicolo.casagrande7@gmail.com>
2024-03-09 15:47:43 +0100

    fix works.ts

--- src/app/works/works.component.ts
+++ src/app/works/works.component.ts
@@ -15,7 +15,7 @@ export class WorksComponent implements OnInit{
   works: Work[] = [];
 
   ngOnInit(){
-    this.works.push(new Work('Internal Chat Reproduction', 'Creation of an Enterprise chat for employee only', 'https://nicolocasagrande.github.io/chat-reactjs/', 'https://github.com/NicoloCasagrande/chat-reactjs'));
+    this.works.push(new Work('Enterprise Chat Reproduction', 'Creation of an Enterprise chat for employee only', 'https://nicolocasagrande.github.io/chat-reactjs/', 'https://github.com/NicoloCasagrande/chat-reactjs'));
     this.works.push(new Work('Generic Dashboard', 'Creation of a generic dashboard template.', 'https://darling-buttercream-2e256f.netlify.app/', 'https://github.com/NicoloCasagrande/html-css-bootstrap-dashboard'));
     this.works.push(new Work('Zoom Reproduction', 'Reproduction of a Zoom videocall.', 'https://silver-paletas-9caf29.netlify.app/', 'https://github.com/NicoloCasagrande/html-css-zoom'));
     this.works.push(new Work('Whatsapp Reproduction', 'Reproduction of Whatsapp using an API that response with Chuck Norris phrases.', 'https://melodious-marshmallow-b9191a.netlify.app/', 'https://github.com/NicoloCasagrande/vue-boolzapp'));
